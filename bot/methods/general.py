0a077fbfe3fd878e212e8f103776af6f55a06ad8
DefiPhigo <philipp.golda@googlemail.com>
2025-02-15 18:17:48 +0100

    add coming soon message

--- bot/methods/general.py
+++ bot/methods/general.py
@@ -3,6 +3,7 @@ from telegram.ext import ContextTypes
 
 from bot.logger import logger
 from bot.buttons import MAIN_MARKUP
+from bot.methods.utils import Utils
 
 """ General Methods """
 
@@ -12,39 +13,43 @@ class GeneralMethods:
     async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         logger.info(f"Start from {update.message.from_user.username}")
 
-        welcome_message = """
-🎉 Welcome to Frankencoin Bot! 🤖
-
-- 🏦 Your gateway to the Frankencoin protocol:
-- 📊 Monitor positions
-- 💰 Check rates
-- 🔔 Get notifications
-
-Need help? Type /help 💡
-
-Let's get started! 🚀
-        """
+        message = (
+            "🎉 Welcome to Frankencoin Bot\\! 🤖 \n\n"
+            "🏦 Your gateway to the Frankencoin protocol: \n"
+            "📊 Monitor positions \n"
+            "💰 Check rates \n"
+            "🔔 Get notifications \n\n"
+            
+            "Need help? Type /help 💡 \n\n"
+            "Let's get started\\! 🚀"
+        )
 
         # Option 1: Send image from local file
         with open('../media/frankencoin_telegram_bot_logo_black.png', 'rb') as photo:
             await update.message.reply_photo(
                 photo=photo,
-                caption=welcome_message,
+                caption=message,
                 reply_markup=MAIN_MARKUP
             )
+
+    @staticmethod
+    async def coming_soon(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        logger.info(f"Unknown command from {update.message.from_user.username}: {update.message.text}")
+
+        message = (
+            "🚀 This function will be available soon\\!"
+        )
+
+        await Utils.send_msg(update, [message], None, True)
 
     @staticmethod
     async def unknown_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         logger.info(f"Unknown command from {update.message.from_user.username}: {update.message.text}")
 
-        unknown_message = """
-❓ Oops! I don't know this command.
-
-🔍 Try /help to see all available commands!
-
-Or use the menu below 👇
-        """
-
-        await update.message.reply_text(
-            text=unknown_message
+        message = (
+            "❓ Oops! I don't know this command. \n"
+            "🔍 Try /help to see all available commands. \n\n"
+            "Or use the menu below 👇"
         )
+
+        await Utils.send_msg(update, [message], None, True)
